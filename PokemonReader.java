10080ac22b8e8fe18d8d45ca9984e98dd63eadc0
bwboyle <brendanboyle02@gmail.com>
2021-03-20 15:08:23 -0500

    bug fixes

--- PokemonReader.java
+++ PokemonReader.java
@@ -22,11 +22,30 @@ public class PokemonReader implements PokemonReaderInterface{
 		String[] text = readInputReader(inputFileReader).split("\n");
 		int cutoff = text[0].split(",").length;
 		for (int i = 1; i < text.length; i++) {
-			ArrayList<Object> info = parsePokemon(text[i]);
+			ArrayList<String> info = parsePokemon(text[i]);
 			if (info.size() != cutoff) {
 				throw new DataFormatException("Invalid Number of Columns");
 			}
-			Pokedex.add(new Pokemon((int)info.get(0),(String)info.get(1),(String)info.get(2),(String)info.get(3),(int)info.get(4),(int)info.get(5),(int)info.get(6),(int)info.get(7),(int)info.get(8),(int)info.get(9),(int)info.get(10),(int)info.get(11),(boolean)info.get(12)));
+
+			
+			//Pokedex.add(new Pokemon((int)info.get(0),(String)info.get(1),(String)info.get(2),(String)info.get(3),(int)info.get(4),(int)info.get(5),(int)info.get(6),(int)info.get(7),(int)info.get(8),(int)info.get(9),(int)info.get(10),(int)info.get(11),(boolean)info.get(12)));
+			
+			Pokedex.add(new Pokemon(
+				Integer.parseInt(info.get(0)),
+				info.get(1),
+				info.get(2),
+				info.get(3),
+				Integer.parseInt(info.get(4)),
+				Integer.parseInt(info.get(5)),
+				Integer.parseInt(info.get(6)),
+				Integer.parseInt(info.get(7)),
+				Integer.parseInt(info.get(8)),
+				Integer.parseInt(info.get(9)),
+				Integer.parseInt(info.get(10)),
+				Integer.parseInt(info.get(11)),
+				Boolean.valueOf(info.get(12))
+			));
+
 		}
 		return Pokedex;
 	}
@@ -40,11 +59,11 @@ public class PokemonReader implements PokemonReaderInterface{
 		return toReturn.toString();
 	}
 
-	public ArrayList<Object> parsePokemon(String e) {
+	public ArrayList<String> parsePokemon(String e) {
 		Object[] text = e.split(",");
-		ArrayList<Object> toReturn = new ArrayList<Object>();
+		ArrayList<String> toReturn = new ArrayList<String>();
 		for(int i = 0; i < text.length; i++) {
-			toReturn.add(text[i]);
+			toReturn.add(String.valueOf(text[i]));
 		}
 		return toReturn;
 	}
